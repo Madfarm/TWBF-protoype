1f8e74726da5e8be8764a331ce649c59613d2176
AnthonyScott <anthony.scott1005@gmail.com>
2024-09-22 16:31:42 -0400

    Style the choose page

--- app/routes/choose.tsx
+++ app/routes/choose.tsx
@@ -2,9 +2,20 @@ import { Link } from "@remix-run/react"
 
 export default function ChoosePage() {
     return (
-        <main>
-            <Link to="/learn">left</Link>
-            <Link to="/guidance">right</Link>
+        <main className="flex flex-row w-screen h-screen">
+            <Link className="choose-container" to="/learn">
+                <div 
+                    className="bg-[url('/learn-background.jpg')] choose-image" 
+                />
+                <h1 className="choose-text">LEARN</h1>
+            </Link>
+            
+            <Link className="choose-container" to="/learn">
+                <div 
+                    className="bg-[url('/guidance-background.jpg')] choose-image" 
+                />
+                <h1 className="choose-text">AI Assistant</h1>
+            </Link>
         </main>
     )
 }
