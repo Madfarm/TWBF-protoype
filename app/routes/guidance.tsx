a5db42d5ffe84c40e06ed124b6ffecb05a8ff0c5
AnthonyScott <anthony.scott1005@gmail.com>
2024-09-22 17:09:59 -0400

    Protect the Learn and Guidance routes

--- app/routes/guidance.tsx
+++ app/routes/guidance.tsx
@@ -1,3 +1,14 @@
+import type { LoaderFunctionArgs } from "@remix-run/node"
+import authenticator from "~/lib/auth.server"
+
+export const loader = async({
+    request,
+}: LoaderFunctionArgs) => {
+    return await authenticator.isAuthenticated(request, {
+        failureRedirect: "/login"
+    })
+}
+
 export default function GuidancePage() {
     return (
         <main>
--- app/routes/learn.tsx
+++ app/routes/learn.tsx
@@ -1,6 +1,18 @@
+import type { LoaderFunctionArgs } from '@remix-run/node'
+import authenticator from '~/lib/auth.server'
 import {
     Outlet
 } from '@remix-run/react'
+
+export const loader = async({
+    request,
+}: LoaderFunctionArgs) => {
+    return await authenticator.isAuthenticated(request, {
+        failureRedirect: "/login"
+    })
+}
+
+
 
 export default function LearnLayout() {
     return (
